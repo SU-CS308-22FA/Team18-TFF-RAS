eb3770721f68004c7e8a0e196932aade47ee7e93
stevenelkhaldi30 <steven.k@eurisko.net>
2022-11-02 03:50:47 +0300

    Hash passwords in database
    Add hashing to passwords of new users when storing in database to deal with security threats and data leaks.

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err.message);
+  console.log(err);
 
   const defaultError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
@@ -19,7 +19,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
   }
 
-  // res.status(defaultError.statusCode).json({ msg: err });
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
