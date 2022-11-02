22fa0f432f23c4a6a4f16645e31a7bb6ea3a6c47
stevenelkhaldi30 <steven.k@eurisko.net>
2022-11-02 21:16:52 +0300

    small fix

--- server.js
+++ server.js
@@ -58,12 +58,12 @@ app.get("*", (req, res) => {
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log(`Server is listening on port ${port}...`);
     });
   } catch (error) {
