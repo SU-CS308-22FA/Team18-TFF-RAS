6c69f35a2f3c3f13ac78e8f14711387f164c42e5
Kerim Demir <kerim.demir2002@hotmail.com>
2022-11-14 18:25:48 +0300

    chore: add objection to referee uı

---
+++ client/src/pages/Objections.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+// JS
+// const input = document.getElementById('myText');
+// const inputValue = input.value
+// React
+// value, onChange
+
+const GetObjection = () => {
+  const [objection, setObjection] = useState({
+    teamName: "",
+    referee: "",
+    objectionText: ""
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setObjection({ ...objection, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (objection.teamName && objection.referee && objection.objectionText) {
+      setObjection({ teamName: "", referee: "", objectionText: "" });
+    }
+  };
+  return (
+    <>
+      <article className="form">
+        <form>
+          <div className="form-control">
+            <label htmlFor="teamName">Team name: </label>
+            <input
+              type="text"
+              id="teamName"
+              name="teamName"
+              value={objection.teamName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Referee name: </label>
+            <input
+              type="text"
+              id="referee"
+              name="referee"
+              value={objection.referee}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="age">Objection: </label>
+            <input
+              type="text"
+              id="objectionText"
+              name="objectionText"
+              value={objection.objectionText}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn" onClick={handleSubmit}>
+            Submit
+          </button>
+        </form>
+      </article>
+    </>
+  );
+};
+
+export default GetObjection;
