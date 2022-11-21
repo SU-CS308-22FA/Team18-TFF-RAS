9eadf980d0b09a92ec19f39916877104890354b3
Kerim Demir <kerim.demir2002@hotmail.com>
2022-11-21 12:08:07 +0300

    chore: objection controller implemented

---
+++ controllers/objectionController.js
@@ -0,0 +1,42 @@
+import Objection from "../models/Objection";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+
+const createObjection = async (req, res) => {
+  const { clubId, refereeId, obejction, isInProcess, isResolved } = req.body;
+
+  if (!clubId || !refereeId || !obejction) {
+    throw new BadRequestError("please provide all values");
+  }
+
+
+  const objection = await Objection.create({ clubId, refereeId, obejction, isInProcess, isResolved });
+
+  res.status(StatusCodes.CREATED).json({
+    objection: {
+      clubId: objection.clubId,
+      refereeId: objection.refereeId,
+      objection: objection.objection,
+      isInProcess: objection.isInProcess,
+      isResolved: objection.isResolved,
+    }
+  });
+};
+
+const getObjections = async (req, res) => {
+    const objections = await Objection.find({}.toArray(function(err, result){ 
+        if (err) throw err;
+        console.log(result);
+    }));
+}
+
+
+const deleteObjection = async (req, res) => {
+  const objection = await Objection.findOne({ _id: req.objection.objectionId });
+
+  await objection.remove();
+
+  res.status(StatusCodes.OK).json({ msg: "Success! Objection removed" });
+};
+
+export { createObjection, getObjections, deleteObjection };
