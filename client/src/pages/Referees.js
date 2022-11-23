ebb3c57df987b4bafff99c42cb9034bf5c55ed0b
Kerim Demir <kerim.demir2002@hotmail.com>
2022-11-23 18:00:41 +0300

    referee information can be processed

--- client/src/pages/Referees.js
+++ client/src/pages/Referees.js
@@ -1,57 +1,72 @@
 import React, { useState, useEffect } from "react";
-import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
-import { useAppContext } from "../context/appContext";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
-
-
 
 
 const Referees = () => {
 
-  const [ref, setRef] = useState({});
-  const [matches, setMatches] = useState([]);
-  const [home, setHome] = useState("");
+  const [refName, setRefName] = useState("");
+  const [license, setLicense] = useState("");
+  const [games, setGames] = useState([]);
+  const [classification, setClassification] = useState("");
+  const [region, setRegion] = useState("");
   const [showButton, setShowButton] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const baseURL = "/api/referee/20160";
-  //const id = "20160";
+  const baseURL = "/api/referee/";
+  const id = "20160";
 
   async function getRefereeData() {
 	try {
-		const response = await axios.get(`${baseURL}`);
-    setRef(response.data);
+    setIsLoading(true);
+    setShowButton(false);
+		const response = await axios.get(`${baseURL + id}`);
+    const data = response.data;
+    console.log("data: ", data);
+    setRefName(data["name"]);
+    setGames(data["matchesRuled"]);
+    setClassification(data["classification"]);
+    setRegion(data["region"]);
+    setLicense(data["licenceNumber"])
+    setIsLoading(false);
 	}
 	catch (error) {
-		console.log(error);
+		console.log(error)
+    setIsLoading(false);
 	}
 }
 
 const handleClick = () => {
-  getRefereeData();
-  setShowButton(false)
+  getRefereeData();;
 }
 
-  // useEffect(() => {
-  //   getRefereeData();
-  // },[])
 
-
-  // useEffect(() => {
-  //   axios.get(`${baseURL}`).then((response) => {
-  //     setRef(response.data);
-  //   }).catch(function(error) {
-  //     console.log("There is an error")
-  //   })
-  // }, []);
+const DisplayMatches = () => {
+  return (
+    games.map((game) => {
+        return ( <div>
+          <h1>home: {game["home"]}</h1> 
+          <h2>away: {game["away"]}</h2>
+          <h3>score: {game["score"]}</h3>
+          <p>--------------------------------</p>
+        </div>
+        )
+      }
+    )
+  )
+}
 
   return (
     <Wrapper className="full-page">
       <div className="form">
         {showButton?<button className="btn" onClick={handleClick}>
-        click to get referee</button>: ref["name"]}</div>
+        click to get referee</button>: isLoading?  "Referee information is loading...":
+        <div> 
+        name: {refName}
+        region: {region}
+        game1: {games.length > 0  ? games[0].home : "not yet"}
+        </div>
+        }</div>
     </Wrapper>
   )
 };
