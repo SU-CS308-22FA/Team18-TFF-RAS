9f800f3561c6deb93debdbf7460315e87416822b
stevenelkhaldi30 <steven.k@eurisko.net>
2022-12-07 23:04:32 +0300

    feat: add fan rating UI

--- client/src/assets/wrappers/MatchPage.js
+++ client/src/assets/wrappers/MatchPage.js
@@ -112,6 +112,190 @@ const Wrapper = styled.main`
     margin-bottom: 15px;
     width: 80%;
   }
+
+  .events-reviews-container {
+    padding-top: 0px;
+    /* padding-right: 20px; */
+    padding-bottom: 0px;
+    /* padding-left: 20px; */
+
+    display: grid;
+    max-height: 700px;
+    overflow-y: scroll;
+
+    width: 80%;
+  }
+
+  .event-review-container {
+    display: grid;
+    height: fit-content;
+    row-gap: 10px;
+    column-gap: 20px;
+    padding-top: 10px;
+    padding-bottom: 20px;
+    padding-right: 0px;
+    padding-left: 0px;
+    max-height: 1000px;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    font-size: 16px;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: rgba(200, 200, 200, 0.5);
+  }
+
+  .events-reviews-container .event-review-container:last-of-type {
+    border-bottom: none;
+  }
+
+  .event-review-details-container {
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+  }
+
+  .event-review-time {
+    margin-right: 11px;
+    height: 16px;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    font-weight: bolder;
+  }
+
+  .event-review-container span {
+    color: rgba(97, 223, 110, 1);
+  }
+
+  .event-review-time span:first-of-type {
+    justify-self: center;
+  }
+
+  .event-review-time span {
+    font-size: 16px;
+  }
+
+  .event-review-container [class*="event-review-title"] {
+    color: rgba(97, 223, 110, 1);
+  }
+
+  .event-review-title {
+    font-size: 16px;
+    line-height: 1;
+    letter-spacing: 0.54px;
+
+    display: block;
+    font-size: 0.83em;
+    /* margin-block-start: 1.67em;
+    margin-block-end: 1.67em; */
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+  }
+
+  .event-review-container a,
+  .event-review-container button {
+    -webkit-text-decoration: none;
+    text-decoration: none;
+    color: var(--GlobalColorScheme-Text-textDefault);
+    outline: none;
+    border: 0;
+    background: 0;
+    cursor: pointer;
+    padding: 0;
+  }
+
+  .event-review-player-card-container:hover {
+    background-color: rgba(200, 200, 200, 0.2);
+  }
+
+  .event-review-player-card-container {
+    display: flex;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    /* margin-top: 12px; */
+    padding: 10px;
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+    border-bottom-right-radius: 12px;
+    border-bottom-left-radius: 12px;
+    height: 70px;
+    border-top-color: rgba(200, 200, 200, 0.5);
+    border-top-style: solid;
+    border-top-width: 1px;
+    border-right-color: rgba(200, 200, 200, 0.5);
+    border-right-style: solid;
+    border-right-width: 1px;
+    border-bottom-color: rgba(200, 200, 200, 0.5);
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+    border-left-color: rgba(200, 200, 200, 0.5);
+    border-left-style: solid;
+    border-left-width: 1px;
+    -webkit-box-align: center;
+    align-items: center;
+
+    width: 100%;
+  }
+
+  .event-review-player-title-details {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    -webkit-box-align: center;
+    align-items: center;
+  }
+
+  .player-icon-css .review-team-icon {
+    position: absolute;
+    right: 0px;
+    bottom: 0px;
+  }
+
+  img.Image.review-team-icon {
+    width: 16px;
+    aspect-ratio: auto 16 / 16;
+    height: 16px;
+  }
+
+  .event-review-player-name-container {
+    display: grid;
+    margin-left: 15px;
+  }
+
+  .event-review-player-first-name {
+    letter-spacing: 0.2px;
+    font-size: 16px;
+    line-height: 1.23;
+    text-align: left;
+    color: black !important;
+  }
+
+  .event-review-player-last-name {
+    letter-spacing: 0.2px;
+    font-size: 16px;
+    line-height: 1.23;
+    text-align: left;
+    margin-top: 2px;
+    color: black !important;
+  }
+
+  .event-review-player-card-container svg {
+    height: 28px;
+    width: 28px;
+  }
+
+  .review-event-comment {
+    margin-top: 10px;
+    width: 100%;
+  }
+
+  .reviews-divider {
+    width: 100%;
+    height: 1.5px;
+    background-color: rgba(200, 200, 200, 0.8);
+    margin-bottom: 10px;
+    margin-top: 5px;
+  }
 `;
 
 export default Wrapper;
--- client/src/pages/dashboard/match/Match.js
+++ client/src/pages/dashboard/match/Match.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import GoalEventIcon from "../../../assets/images/goal-event-icon.svg";
+
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
@@ -62,6 +64,24 @@ const Match = () => {
 
   return (
     <MatchPageWrapper>
+      {/* <div style={{ width: "100%", height: 100, position: "relative" }}>
+        <iframe
+          src="https://www.scorebat.com/embed/v/63900a02a0901/?utm_source=api&utm_medium=video&utm_campaign=dflt"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+          allowfullscreen
+          allow="autoplay; fullscreen"
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            left: 0,
+            top: 0,
+            overflow: "hidden",
+          }}
+        ></iframe>
+      </div> */}
       <div
         className="overlay"
         style={{ display: isChoosingEvent ? "block" : "none" }}
@@ -108,6 +128,66 @@ const Match = () => {
                       variant="filled"
                       color="primary"
                     />
+                    {reviewEvents.length > 0 ? (
+                      <div className="reviews-divider" />
+                    ) : null}
+                    <div className="events-reviews-container">
+                      {reviewEvents.map((reviewEvent, idx) => (
+                        <div key={idx} className="event-review-container">
+                          <div className="event-review-details-container">
+                            <div className="event-review-time">
+                              <span>88’ Goal!</span>
+                            </div>
+                            {/* <h5 className="event-review-title">Goal!</h5> */}
+                          </div>
+                          <a>
+                            <div className="event-review-player-card-container">
+                              <div className="event-review-player-title-details">
+                                <div
+                                  className="player-icon-css"
+                                  width="40"
+                                  height="40"
+                                >
+                                  <img
+                                    alt=""
+                                    className="Image PlayerImage"
+                                    width="40"
+                                    height="40"
+                                    src="https://images.fotmob.com/image_resources/playerimages/291399.png"
+                                  />
+                                  <img
+                                    alt=""
+                                    className="Image review-team-icon"
+                                    width="16"
+                                    height="16"
+                                    src="https://images.fotmob.com/image_resources/logo/teamlogo/8637_xsmall.png"
+                                  />
+                                </div>
+                                <div className="event-review-player-name-container">
+                                  <span className="event-review-player-first-name">
+                                    Abdülkerim
+                                  </span>
+                                  <span className="event-review-player-last-name">
+                                    Bardakci
+                                  </span>
+                                </div>
+                              </div>
+                              <img src={GoalEventIcon} />
+                            </div>
+                          </a>
+                          <TextField
+                            className="review-event-comment"
+                            label="Event Review"
+                            multiline
+                            rows={3}
+                            defaultValue=""
+                            placeholder="Describe the referee's performance in this event"
+                            variant="filled"
+                            color="secondary"
+                          />
+                        </div>
+                      ))}
+                    </div>
                     <Button
                       onClick={() => setIsChoosingEvent(!isChoosingEvent)}
                       className="add-event-review-button"
