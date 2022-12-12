003c002b34cd5844cf9c979fa5025ea3eb992c22
Kerim Demir <kerim.demir2002@hotmail.com>
2022-12-12 18:09:44 +0300

    chore: template fetch

--- client/src/pages/dashboard/RefereeAssignment.js
+++ client/src/pages/dashboard/RefereeAssignment.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 
 const RefereeAssignment = () => 
 {
+    const [ratings, setRatings] = useState([])
+
+    async function getRefereeRatings() {
+	try {
+		const response = await axios.get(`${baseURL + id}`);
+    const data = response.data;
+        // assign the data to somewhere 
+    }
+	catch (error) {
+		console.log(error)
+	}
+}
+
 
     return (
         <div>Assign me</div>
