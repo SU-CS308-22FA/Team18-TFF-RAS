65878f55f69e7dde0e62a0a4fa73b08cd57b6c6c
Kerim Demir <kerim.demir2002@hotmail.com>
2022-12-27 12:22:40 +0300

    Fix page swap for assignemnt page
    added a button that the user can click on one and swap the page between matches and referees.

--- client/src/pages/dashboard/RefereeAssignment.js
+++ client/src/pages/dashboard/RefereeAssignment.js
@@ -1,20 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import Wrapper from "../../assets/wrappers/RegisterPage";
 import axios from "axios"
-import { Link } from "react-router-dom";
-import { referees } from "../../utils/constants";
-
+import { ButtonGroup } from '@mui/material';
+import { Button } from '@mui/material';
 const RefereeAssignment = () => 
 {
-
-  const [rating, setRating] = useState({
-    referee: "",
-    refereeId: "",
-    avgRating: 0,
-    fanRating: 0,
-    expertRating: 0,
-    reviews: []
-  })
+  const [display, setDisplay] = useState({
+    showOptions: true,
+    showRefs: false,
+    showMathces: false,
+  }) 
 
   const [allRatings, setAllRatings] = useState([]);
   
@@ -35,11 +30,26 @@ const RefereeAssignment = () =>
     // })
   }, [])
   //------------------------
+
+  const handleRefClick = (num) => {
+    if(num === 0) // refs
+      setDisplay({showOptions: false, showRefs: true, showMathces: false});
+    else if(num === 1) 
+      setDisplay({showOptions: false, showRefs: false, showMathces: true});
+
+  }
     
     return (
       <Wrapper className="full-page">
+          <ButtonGroup fullWidth={true} disableElevation={true} variant="contained" aria-label="outlined primary button group" style={{ maxWidth: '500px' }}>
+            <Button onClick={() => handleRefClick(0)}>Referees</Button>
+            <Button onClick={() => handleRefClick(1)}>Matches</Button>
+          </ButtonGroup>
       <div className="container-ref">
-          {allRatings.map((ref) => {
+        {/* <div> */}
+        {/* </div> */}
+        {display.showOptions? <h1>True</h1>: <h1>False</h1>}
+          {/* {allRatings.map((ref) => {
             return (
               <div key={ref._id} className="form-ref">
                 <h5>{ref.referee} </h5>
@@ -48,7 +58,7 @@ const RefereeAssignment = () =>
             </button>
               </div>
             );
-          })}      
+          })}       */}
       </div>
     </Wrapper>
     )
