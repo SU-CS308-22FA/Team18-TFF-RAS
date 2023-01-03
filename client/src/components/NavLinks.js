50a794b57c80a9641d1fd77003569eabdfe79f71
stevenelkhaldi30 <steven.k@eurisko.net>
2023-01-03 14:27:50 +0300

    Show nav links based on user type
    Hide nav links depending on the current user type and disable functionalities in pages depending on the user type.

--- client/src/components/NavLinks.js
+++ client/src/components/NavLinks.js
@@ -7,10 +7,12 @@ const NavLinks = ({ toggleSidebar }) => {
   const { user } = useAppContext();
   let newLinks = [...links];
 
-  if (user.type === "expert") {
-    newLinks = links.filter(
-      (link) => link.path !== "objection" && link.path !== "reports"
-    );
+  if (!["expert", "club", "investigator"].includes(user?.type)) {
+    newLinks = newLinks.filter((link) => link.path !== "objection");
+  }
+
+  if (!["observer"].includes(user?.type)) {
+    newLinks = newLinks.filter((link) => link.path !== "reports");
   }
 
   return (
