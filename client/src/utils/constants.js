25db956f4b4e75105001d97166565800dccd254e
Ahmet Alperen Öznam <aoznam@sabanciuniv.edu>
2023-01-07 12:53:33 +0300

    Fix: Review adding for some rferees was giving error.
    RapidAPI refname and TFF rafname are not matching constants file updated.

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -9,42 +9,42 @@ const referees = [
   },
   {
     name: "Yaşar Kemal Uğurlu",
-    apiName: ["Y. Uğurlu"],
+    apiName: ["Y. Uğurlu", "Yasar Kemal Ugurlu"],
     id: "20554",
     image:
       "https://img.a.transfermarkt.technology/portrait/medium/351-1526478925.jpg?lm=1",
   },
   {
     name: "Yasin Kol",
-    apiName: ["Y. Kol"],
+    apiName: ["Y. Kol", "Yasin Kol"],
     id: "1144469",
     image:
       "https://img.a.transfermarkt.technology/portrait/medium/5172-1592093561.jpeg?lm=1",
   },
   {
     name: "Volkan Bayarslan",
-    apiName: ["V. Bayarslan"],
+    apiName: ["V. Bayarslan", "Volkan Bayarslan"],
     id: "20204",
     image:
       "https://img.a.transfermarkt.technology/portrait/big/4569-1592093653.jpeg?lm=1",
   },
   {
     name: "Ümit Öztürk",
-    apiName: ["Ü. Öztürk"],
+    apiName: ["Ü. Öztürk","Umit Ozturk"],
     id: "1091989",
     image:
       "https://img.a.transfermarkt.technology/portrait/big/9552-1591989293.jpeg?lm=1",
   },
   {
     name: "Tugay Kaan Numanoğlu",
-    apiName: ["T. Numanoğlu"],
+    apiName: ["T. Numanoğlu", "Tugay Kaan Numanoglu"],
     id: "1091799",
     image:
       "https://img.a.transfermarkt.technology/portrait/big/9954-1592093506.jpeg?lm=1",
   },
   {
     name: "Suat Arslanboğa",
-    apiName: ["S. Arslanboğa"],
+    apiName: ["S. Arslanboğa", "Suat Arslanboga"],
     id: "19445",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Suat_Arslanbo%C4%9Fa.JPG/640px-Suat_Arslanbo%C4%9Fa.JPG",
@@ -57,14 +57,14 @@ const referees = [
   },
   {
     name: "Mustafa Kürşad Filiz",
-    apiName: ["M. Filiz"],
+    apiName: ["M. Filiz", "Mustafa Kursad Filiz"],
     id: "1091628",
     image:
       "https://i.goalzz.com/?i=o%2Fr%2F7%2F221%2Fmustafa-kursad-filiz-1.jpg",
   },
   {
     name: "Mete Kalkavan",
-    apiName: ["M. Kalkavan"],
+    apiName: ["M. Kalkavan", "Mete Kalkavan"],
     id: "18972",
     image:
       "https://www.tff.org/Resources/TFF/Auto/78875f9f75334638bff3a5aadf58a761.jpg",
@@ -90,47 +90,47 @@ const referees = [
   },
   {
     name: "Halil Umut Meler",
-    apiName: ["H. Meler"],
+    apiName: ["H. Meler", "Halil Umut Meler"],
     id: "21156",
     image:
       "https://www.tff.org/Resources/TFF/Auto/d34cd4b6c1c64fb6a49777e04341da96.png",
   },
   {
     name: "Erkan Özdamar",
-    apiName: ["E. Özdamar"],
+    apiName: ["E. Özdamar", "Erkan Ozdamar"],
     id: "1144690",
     image: "http://kayserispor.gen.tr/uploads/erkan-ozdamar.jpg",
   },
   {
     name: "Çağdaş Altay",
-    apiName: ["Ç. Altay"],
+    apiName: ["Ç. Altay", "Cagdas Altay"],
     id: "21019",
     image:
       "https://img.a.transfermarkt.technology/portrait/big/9821-1491668264.jpeg?lm=1",
   },
   {
     name: "Burak Şeker",
-    apiName: ["B. Şeker"],
+    apiName: ["B. Şeker", "Burak Seker"],
     id: "1090884",
     image: "http://kayserispor.gen.tr/uploads/burak-seker.jpg",
   },
   {
     name: "Bahattin Şimşek",
-    apiName: ["B. Şimşek"],
+    apiName: ["B. Şimşek", "Bahattin Simsek"],
     id: "1140355",
     image:
       "https://img.a.transfermarkt.technology/portrait/big/9565-1592093603.jpeg?lm=1",
   },
   {
     name: "Atilla Karaoğlan",
-    apiName: ["A. Karaoğlan"],
+    apiName: ["A. Karaoğlan", "Atilla Karaoglan"],
     id: "1064324",
     image:
       "https://img.a.transfermarkt.technology/portrait/big/12534-1591989351.jpeg?lm=1",
   },
   {
     name: "Arda Kardeşler",
-    apiName: ["A. Kardeşler"],
+    apiName: ["A. Kardeşler", "Arda Kardesler"],
     id: "1152086",
     image:
       "https://img.a.transfermarkt.technology/portrait/big/9930-1565351940.jpg?lm=1",
@@ -144,7 +144,7 @@ const referees = [
   },
   {
     name: "Ali Palabıyık",
-    apiName: ["A. Palabıyık"],
+    apiName: ["A. Palabıyık", "Ali Palabiyik"],
     id: "20160",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/0/0e/2019-06-11_Fu%C3%9Fball%2C_M%C3%A4nner%2C_L%C3%A4nderspiel%2C_Deutschland-Estland_StP_2082_LR10_by_Stepro.jpg",
